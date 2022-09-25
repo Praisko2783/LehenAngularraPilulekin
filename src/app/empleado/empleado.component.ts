7872649e8af739a540a93fd68ee356bb66d71b60
Praisko2783 <praiskopedia@gmail.com>
2022-09-25 17:25:39 +0200

    lehenAngularra

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+  
+  //template: "<p>Hemen jarriko genituzke langileetako baten jakingarriak</p>",
+  //styles:["p{background:green; padding:5px; color:white}"]
+})
+export class EmpleadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
