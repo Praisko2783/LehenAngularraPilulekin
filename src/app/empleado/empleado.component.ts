2bdb90b8b0a3d0df6b01ddbc68a3015981ad94c2
Praisko2783 <praiskopedia@gmail.com>
2022-09-26 15:22:02 +0200

    hainbat aldaketa: empleado-n eta empleados-en

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   //styles:["p{background:green; padding:5px; color:white}"]
 })
 export class EmpleadoComponent implements OnInit {
+  
+  nombre="Juan";
+  apellido="Biain";
+  edad=17;
+  /*getEdad(){
+    return this.edad;
+  }*/
+  adinadeika(value:String){ }
+  
+  lantokideika(value:String){ }
+  habilitaCuadro=false;
+  
+  
+  usuRegistrado=false;
+  
+  textoDeRegistro="Inor ez da erroldatu, oraindik.";
+  erroldaTestua="Oraindaino, inor EZ DA ERROLDATU, lastima!!.";
 
-  constructor() { }
+  getRegistro(value:String){
+    this.usuRegistrado=false;
+  }
+  setRegistro(event:Event){
+    //alert("Bisitaria erroldatu egin da, oraintxe bertan");
+    if((<HTMLInputElement>event.target).checked){
+      this.textoDeRegistro = "Oraintxe bertan erroldatu dat BAT";
+    }else{
+      this.textoDeRegistro="Hara, joan zaigu erroldatua. KAKA!!!.";
+    }
+  }
+  
+  getRegistrousuario(){
+    this.usuRegistrado=false;
+  }
+  setUsuarioregistrado(event:Event){
+    //alert("Bisitaria erroldatu egin da, oraintxe bertan");
+    if((<HTMLInputElement>event.target).value== "si"){
+      this.erroldaTestua="Erroldatu da BAT, ederto!!";
+    }else{
+      this.erroldaTestua="Ostera be, erroldategia HUTSIK, KAKA ZAHARRA!!!.";
+    }
+    //alert(event.target)
+    //this.textoDeRegistro = "Oraintxe erroldatu dat BAT, ederto!!";
+  }
+  
+  constructor() {
+
+   }
 
   ngOnInit(): void {
+
   }
 
 }
